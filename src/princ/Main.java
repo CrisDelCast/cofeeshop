1f2c5d42ed906393b3d2f74da6f3c39b242883e5
Cristian David Del Castillo Garcia <rocosa2003@gmail.com>
2021-02-27 07:48:18 -0500

    mi ultimo committ

--- src/princ/Main.java
+++ src/princ/Main.java
@@ -1,6 +1,7 @@
 package princ;
 
 import java.io.BufferedReader;
+
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,26 +9,6 @@ import java.io.OutputStreamWriter;
 
 public class Main {
 	public final static String separate = " "; 
-	public static void main(String[] args ) throws IOException {
-		String out = "";
-		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
-		
-		String txin = br.readLine();
-		int line = Integer.parseInt(txin);
-		String [] outnumbers = null;
-		double[] lineAL = null;
-		
-		for (int i = 0;i <line;i++) {
-			outnumbers = br.readLine().split(separate);
-			lineAL = new double[outnumbers.length];
-			lineAL = parsInt(lineAL,line,outnumbers);
-			out += bubbleSort(lineAL)+"\n";
-		}
-		bw.write(out);	
-		bw.close();
-		br.close();	
-	}
 	private static String bubbleSort(double[] lineAL) {
 		 String out;
 	        double average = 0;
@@ -58,12 +39,44 @@ public class Main {
 		
 		
 	
-		return null;
+		return out;
 	}
 	private static double[] parsInt(double[] lineAL, int line, String[] outnumbers) {
+	      if (lineAL != null && outnumbers != null) {
+	            for (int j=0;j<lineAL.length;j++) {
+	                lineAL[j]= Double.parseDouble(outnumbers[j]);          
+	            }
+	            
+	        }
+	        
+	        return lineAL;
 		
 	}
 	
 
 
+
+	
+	public static void main(String[] args ) throws IOException {
+		String out = "";
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+		
+		String txin = br.readLine();
+		int line = Integer.parseInt(txin);
+		String [] outnumbers = null;
+		double[] lineAL = null;
+		
+		for (int i = 0;i <line;i++) {
+			outnumbers = br.readLine().split(separate);
+			lineAL = new double[outnumbers.length];
+			lineAL = parsInt(lineAL,line,outnumbers);
+			out += bubbleSort(lineAL)+"\n";
+		}
+		bw.write(out);	
+		bw.close();
+		br.close();	
+	}
+	
 }
+
